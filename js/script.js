4af688151ca2dfec985468b2c37a980423ec269b
protimsaha <protimsaha750@gmail.com>
2022-02-17 10:11:19 +0600

    main part error

--- js/script.js
+++ js/script.js
@@ -1,19 +1,49 @@
 document.getElementById('calculate-button').addEventListener('click', function () {
-    let income = document.getElementById('income')
-    let incomeValue = income.value
 
-    let totalExpenses = document.getElementById('total-expenses')
-    let totalExpensesValue = totalExpenses.innerText;
+    calculateButton();
 
-    console.log(incomeValue, totalExpensesValue)
-    addValue()
 })
 
-function addValue() {
-    let food = document.getElementById('food').value;
-    let rent = document.getElementById('rent').value;
-    let cloth = document.getElementById('cloth').value;
 
-    let sum = parseInt(food) + parseInt(rent) + parseInt(cloth);
+
+function calculateButton() {
+    let food = document.getElementById('food');
+    let foodValue = food.value;
+
+    let rent = document.getElementById('rent');
+    let rentValue = rent.value;
+
+    let cloth = document.getElementById('cloth');
+    let clothValue = cloth.value;
+
+    if (foodValue < 0 || rentValue < 0 || clothValue < 0) {
+        const onlyPositiveNumber = document.getElementById('only-positive');
+        onlyPositiveNumber.style.display = 'block'
+    }
+    else if (typeof (foodValue) == 'string' || typeof (rentValue) == 'string' || typeof (cloth) == 'string') {
+        const nanMessage = document.getElementById('not-a-number');
+        nanMessage.style.display = 'block'
+    }
+    else {
+        let sum = parseFloat(foodValue) + parseFloat(rentValue) + parseFloat(clothValue);
+
+        let totalExpenses = document.getElementById('total-expenses')
+        totalExpenses.innerText = sum;
+
+        let income = document.getElementById('income');
+        let incomeValue = income.value;
+        let newBalance = parseFloat(incomeValue) - sum;
+        let balance = document.getElementById('balance');
+        balance.innerText = newBalance;
+    }
+
+    // income.value = '';
+    // food.value = '';
+    // rent.value = '';
+    // cloth.value = '';
+}
+
+function errorHandle() {
 
 }
+
